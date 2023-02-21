29f0790f9c156df904c966a3c40994e08d4284ba
divan <hlinakcurbag@gmail.com>
2023-02-21 11:29:48 +0300

    глобальные изменения

--- configure.py
+++ configure.py
@@ -1,5 +1,13 @@
+# для бота и платёжной системы
 BOT_TOKEN = '6089974069:AAGslyieQbUFasP9VyuEAmFLAXcR-vknG9g'
 PAYMENTS_TOKEN = '381764678:TEST:49837'
 name = 'testic_bot'
 tokenyoukassa = ''
 phoneyoukassa = ''
+
+# для базы данных
+HOST = ""
+USER = "postgres"
+PASSWORD = "postgres"
+PORT = 5432
+DB_NAME = "apps"
