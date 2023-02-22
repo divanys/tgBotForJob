59744f23748e38dba035be457655773c3a66d9cc
divan <hlinakcurbag@gmail.com>
2023-02-22 21:56:29 +0300

    на этом наши полномочия всё принты удалены

--- парсинг/test.py
+++ парсинг/test.py
@@ -1,8 +1,12 @@
-import requests
-nameText, linkLink, newLst = ['a', 'b', 'c'], ['1', '2', '3'], ['py', 'th', 'on']
+# import requests
+# nameText, linkLink, newLst = ['a', 'b', 'c'], ['1', '2', '3'], ['py', 'th', 'on']
 
-lst = []
-for i in range(len(linkLink)):
-    lst.append({linkLink[i]: {nameText[i]: newLst[i]}})
+# lst = []
+# for i in range(len(linkLink)):
+#     lst.append({linkLink[i]: {nameText[i]: newLst[i]}})
 
-print(lst)
+# print(lst)
+nameTextFor1Price = ['a   csd s ']
+nameFile = f"парсинг/img/{nameTextFor1Price[0]}.jpg".replace(' ', '')
+
+print(nameFile)
