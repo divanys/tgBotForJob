9e2b04ed87b8db3542cf8100f8120dd8e36a9173
divan <hlinakcurbag@gmail.com>
2023-02-23 17:02:54 +0300

    WHO ARE MOLODEC I AM MOLODEC

--- parsingDir/test.py
+++ parsingDir/test.py
@@ -1,12 +1,9 @@
-# import requests
-# nameText, linkLink, newLst = ['a', 'b', 'c'], ['1', '2', '3'], ['py', 'th', 'on']
+import bdForJogDir.parsing as ps
+import bdForJogDir.function as fn
 
-# lst = []
-# for i in range(len(linkLink)):
-#     lst.append({linkLink[i]: {nameText[i]: newLst[i]}})
 
-# print(lst)
-nameTextFor1Price = ['a   csd s ']
-nameFile = f"парсинг/img/{nameTextFor1Price[0]}.jpg".replace(' ', '')
+dataProduct = [
+(ps.keysALl[0], ps.infoAll[ps.keysALl[0]]['Название'], fn.photoInsert(ps.infoAll[ps.keysALl[0]]['Фотография']), ps.infoAll[ps.keysALl[0]]['Цена'])
+]
 
-print(nameFile)
+print(dataProduct)
